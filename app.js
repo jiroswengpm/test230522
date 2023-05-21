605b9f649386db5b565df689777786aa1a965120
jiroswengpm <125707800+jiroswengpm@users.noreply.github.com>
2023-05-22 06:52:01 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+document.getElementById('add-button').addEventListener('click', function() {
+  var value = document.getElementById('todo-input').value;
+  if (value) addTodo(value);
+});
+
+function addTodo(text) {
+  var list = document.getElementById('todo-list');
+
+  var item = document.createElement('li');
+  item.innerText = text;
+
+  var buttons = document.createElement('div');
+
+  var remove = document.createElement('button');
+  remove.innerText = 'Remove';
+  remove.addEventListener('click', removeTodo);
+  buttons.appendChild(remove);
+
+  item.appendChild(buttons);
+
+  list.appendChild(item);
+}
+
+function removeTodo() {
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+
+  parent.removeChild(item);
+}
